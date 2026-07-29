eb192fbca7266efe037cbd96a8885173ce98a892
Ira <m.ira13kas@gmail.com>
2026-07-29 07:07:34 +0300

    add Dependency Injection

--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿using Application.Common.Interfaces;
+using Infrastructure.Persistence;
+using Infrastructure.Persistence.Repositories;
+using Infrastructure.Services;
+using Infrastructure.Settings;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +14,40 @@ using System.Threading.Tasks;
 
 namespace Infrastructure
 {
-    internal class DependencyInjection
+    public static class DependencyInjection
     {
+        public static IServiceCollection AddInfstructure(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseNpgsql(
+                    configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
+
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+            services.AddScoped<IGameRepository, GameRepository>();
+            services.AddScoped<IAdminRepository, AdminRepository>();
+            services.AddScoped<IFineRepository, FineRepository>();
+            services.AddScoped<IGameParticipantRepository, GameParticipantRepository>();
+            services.AddScoped<INotificationRepository, NotificationRepository>();
+            services.AddScoped<IPointsRepository, PointsRepository>();
+            services.AddScoped<IPushRepository, PushRepository>();
+            services.AddScoped<IQuizmanRepository, QuizmanRepository>();
+            services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
+            services.AddScoped<IReplacementRepository, ReplacementRepository>();
+            services.AddScoped<ISmartRepository, SmartRepository>();
+
+            services.AddScoped<IPasswordHasher, PasswordHasher>();
+            services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
+
+            services.Configure<JwtSettings>(configuration.GetSection("JwtSettings"));
+
+            return services;
+        }
     }
 }
